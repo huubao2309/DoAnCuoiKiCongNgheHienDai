bf0d09c70f62e1a32b473e9184bc97db333db75f
HB_SENPAI\HuuBao <huubao2309@gmail.com>
2016-11-15 10:36:38 +0700

    Update Databse! Add Table __MigrationHistory

---
+++ BaoDatShop/BaoDatShop.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using BaoDatShop.Data.Infrastructure;
+using BaoDatShop.Model.Models;
+
+namespace BaoDatShop.Data.Repositories
+{
+    public interface IProductRepository
+    {
+    }
+
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
