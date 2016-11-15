61651faef344e694c60c45450fcebd8c324b330e
MinhDat <haminhdat01@gmail.com>
2016-11-15 23:25:32 +0700

    Add Migration Database

---
+++ BaoDatShop/BaoDatShop.Data/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace BaoDatShop.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BaoDatShop.Data.BaoDatShopDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BaoDatShop.Data.BaoDatShopDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
--- BaoDatShop/BaoDatShop.Model/Adstract/IAuditable.cs
+++ BaoDatShop/BaoDatShop.Model/Adstract/IAuditable.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace TeduShop.Model.Abstract
+namespace BaoDatShop.Model.Abstract
 {
     public interface IAuditable
     {
