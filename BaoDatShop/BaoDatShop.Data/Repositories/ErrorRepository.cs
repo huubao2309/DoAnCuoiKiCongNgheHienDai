3f62a636710fb228b655097201307093d81e1a78
HB_SENPAI\HuuBao <huubao2309@gmail.com>
2016-11-16 00:42:59 +0700

    Update web api continue...

---
+++ BaoDatShop/BaoDatShop.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using BaoDatShop.Data.Infrastructure;
+using BaoDatShop.Model.Models;
+
+namespace BaoDatShop.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
