815357cfcd2ae3f83137c2e69607a71e5e257307
HB_SENPAI\HuuBao <huubao2309@gmail.com>
2016-11-16 22:21:39 +0700

    Update...

---
+++ BaoDatShop/BaoDatShop.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BaoDatShop.Model.Models;
+using BaoDatShop.Web.Models;
+
+namespace BaoDatShop.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.CreateMap<Post, PostViewModel>();
+            Mapper.CreateMap<PostCategory, PostCategoryViewModel>();
+            Mapper.CreateMap<Tag, TagViewModel>();
+        }
+    }
+}
