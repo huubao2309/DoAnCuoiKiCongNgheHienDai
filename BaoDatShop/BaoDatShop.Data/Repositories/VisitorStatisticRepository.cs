82aa9ace7bd9fb920dcd54641ba85754ba19ea59
MinhDat <haminhdat01@gmail.com>
2016-11-20 03:17:55 +0700

    Unit Test

---
+++ BaoDatShop/BaoDatShop.Data/Repositories/VisitorStatisticRepository.cs
@@ -0,0 +1,16 @@
+﻿using BaoDatShop.Data.Infrastructure;
+using BaoDatShop.Model.Models;
+
+namespace BaoDatShop.Data.Repositories
+{
+    public interface IVisitorStatisticRepository : IRepository<VisitorStatistic>
+    {
+    }
+
+    public class VisitorStatisticRepository : RepositoryBase<VisitorStatistic>, IVisitorStatisticRepository
+    {
+        public VisitorStatisticRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
