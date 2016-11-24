bc9d98ae920e76155b68655bfa8ca56fbff34520
HB_SENPAI\HuuBao <huubao2309@gmail.com>
2016-11-24 10:52:54 +0700

    Start code page user

--- BaoDatShop/BaoDatShop.Data/Migrations/Configuration.cs
+++ BaoDatShop/BaoDatShop.Data/Migrations/Configuration.cs
@@ -1,6 +1,10 @@
-namespace BaoDatShop.Data.Migrations
+﻿namespace BaoDatShop.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Model.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,18 +18,52 @@ namespace BaoDatShop.Data.Migrations
 
         protected override void Seed(BaoDatShop.Data.BaoDatShopDbContext context)
         {
+            CreateProductCategorySample(context);
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new BaoDatShopDbContext()));
+
+            //var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new BaoDatShopDbContext()));
+
+            //var user = new ApplicationUser()
+            //{
+            //    UserName = "BaoDat",
+            //    Email = "BaoDat.international@gmail.com",
+            //    EmailConfirmed = true,
+            //    BirthDay = DateTime.Now,
+            //    FullName = "Technology Education"
+
+            //};
+
+            //manager.Create(user, "123654$");
+
+            //if (!roleManager.Roles.Any())
+            //{
+            //    roleManager.Create(new IdentityRole { Name = "Admin" });
+            //    roleManager.Create(new IdentityRole { Name = "User" });
+            //}
+
+            //var adminUser = manager.FindByEmail("BaoDat.international@gmail.com");
+
+            //manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+
+        }
+
+        private void CreateProductCategorySample(BaoDatShop.Data.BaoDatShopDbContext context)
+        {
+            if (context.ProductCategories.Count() == 0)
+            {
+                List<ProductCategory> listProductCategory = new List<ProductCategory>()
+            {
+                new ProductCategory() { Name="Điện lạnh",Alias="dien-lanh",Status=true },
+                 new ProductCategory() { Name="Viễn thông",Alias="vien-thong",Status=true },
+                  new ProductCategory() { Name="Đồ gia dụng",Alias="do-gia-dung",Status=true },
+                   new ProductCategory() { Name="Mỹ phẩm",Alias="my-pham",Status=true }
+            };
+                context.ProductCategories.AddRange(listProductCategory);
+                context.SaveChanges();
+            }
+
         }
     }
 }
