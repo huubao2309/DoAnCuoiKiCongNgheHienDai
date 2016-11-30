e72adb34463b1d271fb67024c8ea9b0832f8e5a7
HB_SENPAI\HuuBao <huubao2309@gmail.com>
2016-12-01 02:04:23 +0700

    Add auto complete to product page

--- BaoDatShop/BaoDatShop.Web/Controllers/ProductController.cs
+++ BaoDatShop/BaoDatShop.Web/Controllers/ProductController.cs
@@ -1,22 +1,80 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BaoDatShop.Common;
+using BaoDatShop.Model.Models;
+using BaoDatShop.Service;
+using BaoDatShop.Web.Infrastructure.Core;
+using BaoDatShop.Web.Models;
 
 namespace BaoDatShop.Web.Controllers
 {
     public class ProductController : Controller
     {
+        IProductService _productService;
+        IProductCategoryService _productCategoryService;
+        public ProductController(IProductService productService, IProductCategoryService productCategoryService)
+        {
+            this._productService = productService;
+            this._productCategoryService = productCategoryService;
+        }
         // GET: Product
         public ActionResult Detail(int productId)
         {
             return View();
         }
 
-        public ActionResult Category(int id)
+        public ActionResult Category(int id, int page = 1, string sort = "")
         {
-            return View();
+            int pageSize = int.Parse(ConfigHelper.GetByKey("PageSize"));
+            int totalRow = 0;
+            var productModel = _productService.GetListProductByCategoryIdPaging(id, page, pageSize, sort, out totalRow);
+            var productViewModel = Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(productModel);
+            int totalPage = (int)Math.Ceiling((double)totalRow / pageSize);
+
+            var category = _productCategoryService.GetById(id);
+            ViewBag.Category = Mapper.Map<ProductCategory, ProductCategoryViewModel>(category);
+            var paginationSet = new PaginationSet<ProductViewModel>()
+            {
+                Items = productViewModel,
+                MaxPage = int.Parse(ConfigHelper.GetByKey("MaxPage")),
+                Page = page,
+                TotalCount = totalRow,
+                TotalPages = totalPage
+            };
+
+            return View(paginationSet);
+        }
+        public ActionResult Search(string keyword, int page = 1, string sort = "")
+        {
+            int pageSize = int.Parse(ConfigHelper.GetByKey("PageSize"));
+            int totalRow = 0;
+            var productModel = _productService.Search(keyword, page, pageSize, sort, out totalRow);
+            var productViewModel = Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(productModel);
+            int totalPage = (int)Math.Ceiling((double)totalRow / pageSize);
+
+            ViewBag.Keyword = keyword;
+            var paginationSet = new PaginationSet<ProductViewModel>()
+            {
+                Items = productViewModel,
+                MaxPage = int.Parse(ConfigHelper.GetByKey("MaxPage")),
+                Page = page,
+                TotalCount = totalRow,
+                TotalPages = totalPage
+            };
+
+            return View(paginationSet);
+        }
+        public JsonResult GetListProductByName(string keyword)
+        {
+            var model = _productService.GetListProductByName(keyword);
+            return Json(new
+            {
+                data = model
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
