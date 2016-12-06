b1a3bec0e3786ac1df7240bf29b9937210fbd8ff
HB_SENPAI\HuuBao <huubao2309@gmail.com>
2016-12-06 22:47:38 +0700

    Add Shop cart to project

--- BaoDatShop/BaoDatShop.Common/CommonConstants.cs
+++ BaoDatShop/BaoDatShop.Common/CommonConstants.cs
@@ -11,5 +11,8 @@ namespace BaoDatShop.Common
         public const string ProductTag = "product";
         public const string PostTag = "post";
         public const string DefaultFooterId = "default";
+
+        public const string SessionCart = "SessionCart";
+
     }
 }
